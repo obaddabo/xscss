b5b311639ff667e90f8e066200d7b91564af83bd
SIta Dabo <euphoria214rue@gmail.com>
2022-10-21 22:46:41 +0100

    xscss
    this is an update to the xscss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-const { dest, series, watch } = require('gulp');
+
+
+// importing of required files
+const { src, dest, series, watch } = require('gulp');
+// importing of gulp-runtime for sass and importing sass
 const sass = require('gulp-sass')(require('sass'));
 
+
+// the first task runner
 function buildStyles(){
-    return src('index.scss')
+    // return the base or the main scssfile
+    return src('xscss/**/*.scss')
+    // compile the scss through the sass function
     .pipe(sass())
+    // send the compiled version to the css folder
     .pipe(dest('css'))
 }
 
+// automated taskrunner
 function taskrunner() {
-
-        watch(['index.scss'], buildStyles())
+        // watch all the scss files for changes
+        watch(['xscss/**/*.scss'], buildStyles)
 }
 
 
